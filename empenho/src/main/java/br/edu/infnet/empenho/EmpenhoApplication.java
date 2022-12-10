32b50feac64502357b350f0da0f59f67dd5d38a9
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-12-10 11:23:12 -0300

    Arquitetura de Microsserviços Java

--- empenho/src/main/java/br/edu/infnet/empenho/EmpenhoApplication.java
+++ empenho/src/main/java/br/edu/infnet/empenho/EmpenhoApplication.java
@@ -2,9 +2,22 @@ package br.edu.infnet.empenho;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class EmpenhoApplication {
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpenhoApplication.class, args);
