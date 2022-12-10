2864ad1d1d8cb6d3ea6df58be572ef7a9cb40b5f
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-12-10 15:58:15 -0300

    Arquitetura de Microsserviços Java

--- fornecedor/src/main/java/br/edu/infnet/fornecedor/modelo/service/FornecedorService.java
+++ fornecedor/src/main/java/br/edu/infnet/fornecedor/modelo/service/FornecedorService.java
@@ -2,6 +2,7 @@ package br.edu.infnet.fornecedor.modelo.service;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,20 @@ public class FornecedorService {
 	
 	@Autowired
 	private FornecedorRepository fornecedorRepository;
+	
+    public void incluir(Fornecedor fornecedor) {
+        fornecedorRepository.save(fornecedor);
+    }
+    
+    public void alterar(Long id, Fornecedor fornecedor) {
+    	Fornecedor fornecedorSalvo = fornecedorRepository.findById(id).orElse(null);
+        BeanUtils.copyProperties(fornecedor, fornecedorSalvo, "id");
+        fornecedorRepository.save(fornecedorSalvo);
+    }
+    
+    public void excluir(Long id) {
+    	fornecedorRepository.deleteById(id);
+    }    
 	
 	public List<Fornecedor> listarTodos(){
 		return (List<Fornecedor>) fornecedorRepository.findAll(); 
