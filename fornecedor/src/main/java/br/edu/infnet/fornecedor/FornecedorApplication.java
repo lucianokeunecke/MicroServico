6e5f56e2214f5bfe8954d0fa28434b6280587f9b
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-12-10 11:14:31 -0300

    Arquitetura de Microsserviços Java

--- fornecedor/src/main/java/br/edu/infnet/fornecedor/FornecedorApplication.java
+++ fornecedor/src/main/java/br/edu/infnet/fornecedor/FornecedorApplication.java
@@ -2,8 +2,10 @@ package br.edu.infnet.fornecedor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FornecedorApplication {
 
 	public static void main(String[] args) {
--- produto/src/main/java/br/edu/infnet/produto/ProdutoApplication.java
+++ produto/src/main/java/br/edu/infnet/produto/ProdutoApplication.java
@@ -2,8 +2,10 @@ package br.edu.infnet.produto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProdutoApplication {
 
 	public static void main(String[] args) {
