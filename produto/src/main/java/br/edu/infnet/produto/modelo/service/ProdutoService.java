c3f0be1944af149df0d4428fe9fee5576e48cc37
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-12-10 16:12:20 -0300

    Arquitetura de Microsserviços Java

--- produto/src/main/java/br/edu/infnet/produto/modelo/service/ProdutoService.java
+++ produto/src/main/java/br/edu/infnet/produto/modelo/service/ProdutoService.java
@@ -2,6 +2,7 @@ package br.edu.infnet.produto.modelo.service;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,20 @@ public class ProdutoService {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+    public void incluir(Produto produto) {
+        produtoRepository.save(produto);
+    }
+    
+    public void alterar(Long id, Produto produto) {
+    	Produto produtoSalvo = produtoRepository.findById(id).orElse(null);
+        BeanUtils.copyProperties(produto, produtoSalvo, "id");
+        produtoRepository.save(produtoSalvo);
+    }
+    
+    public void excluir(Long id) {
+    	produtoRepository.deleteById(id);
+    } 	
 	
 	public List<Produto> listarTodos(){
 		return (List<Produto>) produtoRepository.findAll(); 
